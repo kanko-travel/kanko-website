f676325ca17d1482e4eb4de00b9e890b16e13bf8
Umran Hussain <1547890+umran@users.noreply.github.com>
2024-11-05 01:55:18 +0000

    updated layout

--- src/app/early-access/page.tsx
+++ src/app/early-access/page.tsx
@@ -11,21 +11,21 @@ export default function Home() {
       <main className="">
         <div className="mt-16 w-full">
           <Container>
-            <div className="mx-auto max-w-2xl py-16">
-              <div className="rounded-lg border border-slate-200 px-12 py-8">
-                <div className="mb-8 flex flex-col gap-2">
-                  <h1 className="text-2xl font-semibold md:text-3xl lg:text-4xl">
-                    Sign up for early access
-                  </h1>
-                  <span className="text-lg md:text-xl lg:text-2xl">
-                    Fill out the form below to gain exclusive access to new
-                    features before everyone else
-                  </span>
-                </div>
-                <Suspense>
-                  <EarlyAccessForm />
-                </Suspense>
+            <div className="mx-auto max-w-xl py-16">
+              {/* <div className="rounded-lg border border-slate-200 px-12 py-8"> */}
+              <div className="mb-8 flex flex-col gap-2">
+                <h1 className="text-2xl font-semibold md:text-3xl lg:text-4xl">
+                  Sign up for early access
+                </h1>
+                <span className="text-lg md:text-xl lg:text-2xl">
+                  Fill out the form below to gain exclusive access to new
+                  features before everyone else
+                </span>
               </div>
+              <Suspense>
+                <EarlyAccessForm />
+              </Suspense>
+              {/* </div> */}
             </div>
           </Container>
         </div>
